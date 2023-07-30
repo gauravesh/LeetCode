10edd14f8f4bae54cbb260544e04784131e1c469
Gauravesh Sharma <gauravesh.is20@bmsce.ac.in>
2023-07-30 13:12:38 +0530

    Create Problem List.py
    Given an integer x, return true if x is a 
    palindrome
    , and false otherwise.

---
+++ easy/Problem List.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        x_str=str(x)
+    
+
+        for i in range(len(x_str)):
+            if(x_str[i] != x_str[len(x_str)-1-i]):
+                #print("false")
+                return False
+        
+        return True
+
+            
+           
+        
