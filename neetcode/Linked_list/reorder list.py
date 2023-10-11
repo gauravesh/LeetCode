9ea946c761c8417a518bf75403665865c3769768
Gauravesh Sharma <gauravesh.is20@bmsce.ac.in>
2023-10-11 07:09:20 +0530

    reorder list.py

--- neetcode/Linked_list/reorder list.py
+++ neetcode/Linked_list/reorder list.py
@@ -9,56 +9,33 @@ class Solution(object):
         :type head: ListNode
         :rtype: None Do not return anything, modify head in-place instead.
         """
-        list1=self.copy_list(head)
-        list3=self.copy_list(head)
+        s=f=head
+        while f and f.next:
+            f=f.next.next
+            s=s.next
+        second=s
+        prev=s=None
+        while second:
+            temp=second.next
+            second.next=prev
+            prev=second
+            second=temp
+        
+        f1=head
+        f2=prev
 
-        list2=self.reverse(list3)
-        index=0
-        final_list=ListNode()
-        cur=final_list
-        for i in range(self.length(head)):
-            if i % 2 == 0:
-                temp_node=ListNode(list1.val)
-                temp_node.next=final_list
-                final_list=temp_node
-                list1=list1.next
-            elif i%2==1:
-                temp_node=ListNode(list2.val)
-                temp_node.next=final_list
-                final_list=temp_node
-                list2=list2.next
-        final_list=self.reverse(final_list)
-        cur=final_list.next
-        head=cur
-       
-    def reverse(self, x):
-        cur = x
-        prev = None 
-        while cur:
-            temp = cur.next
-            cur.next = prev
-            prev = cur
-            cur = temp
-        return prev
-    def copy_list(self, y):
-        if not y:
-            return None
-
-        new_head = ListNode(0)  # Create a dummy node to simplify the code.
-        original = y
-        new_tail = new_head  # Initialize new_tail to the dummy node.
-
-        while original:
-            new_node = ListNode(original.val)
-            new_tail.next = new_node
-            new_tail = new_node
-            original = original.next
-
-        return new_head.next  # Return the actual head of the copied list.
-    def length(self,z):
-        cur=z
-        leng=0
-        while cur:
-            cur=cur.next
-            leng+=1
-        return leng
+        while f2:
+            print(f1.val,f2.val)
+            t1=f1.next
+            t2=f2.next
+            f1.next=f2
+            f2.next=t1
+            f1,f2=t1,t2
+        t,h=head,head.next
+        while h and h.next :
+            h=h.next.next
+            t=t.next
+            if h == t:
+                h.next.next=None
+        
+    
