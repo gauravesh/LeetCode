293c3558f95fa73bbdd019558db2e25bde79230c
Gauravesh Sharma <gauravesh.is20@bmsce.ac.in>
2023-10-31 00:29:49 +0530

    Update Binary Search.py

--- neetcode/Binary Search/Binary Search.py
+++ neetcode/Binary Search/Binary Search.py
@@ -7,18 +7,21 @@ class Solution(object):
         """
         l=0
         r=len(matrix)-1
-        mid=l+r//2
-    
-        if target<matrix[0][0] or target > matrix[-1][0]:
+        mid=(l+r)//2
+
+
+        if target<matrix[0][0] or target > matrix[-1][-1]:
             return False
-        while r-l != 0:
+        while l<=r:
             if target == matrix[mid][0]:
                 return True
             if target <matrix[mid][0]:
                 r=mid-1
             if target > matrix[mid][0]:
                 l=mid+1
-            mid=r+l//2
+            mid=(r+l)//2
+
+            
         new=matrix[r]
 
         l=0
@@ -31,7 +34,7 @@ class Solution(object):
                 r=mid-1
             if target > new[mid]:
                 l= mid+1
-            target=(l+r)//2
+            mid=(l+r)//2
         return False
         
 
