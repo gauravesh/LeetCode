556f6cac3b0ff214e2f0a8e113a2e90ce66ed592
Gauravesh Sharma <gauravesh.is20@bmsce.ac.in>
2023-11-02 01:02:26 +0530

    Update Koko Eating Bananas.py

--- neetcode/Binary Search/Koko Eating Bananas.py
+++ neetcode/Binary Search/Koko Eating Bananas.py
@@ -5,23 +5,22 @@ class Solution(object):
         :type h: int
         :rtype: int
         """
-        sample=piles
-        sample.sort()
-        for i in range(h-len(piles)):
-            
-            a=sample[-1]
-            if a%2==0:
-                x=y=(a//2)
+        # Define the search range for Koko's eating speed
+        l, r = 1, max(piles)
 
-            if a%2==1:
-                a=a//2
-                x,y=a,a+1
+        while l < r:
+            mid = (l + r) // 2
+            if self.canFinish(piles, mid, h):
+                # If it's possible to finish in mid speed, try lower speeds
+                r = mid
+            else:
+                # Otherwise, try higher speeds
+                l = mid + 1
 
-            sample=sample[:-1]
-            
-            sample.append(x)
-            sample.append(y)
-            sample.sort()
-            print(sample)
-        sample.sort()
-        return sample[-1]
+        return l
+
+    def canFinish(self, piles, speed, h):
+        hours = 0
+        for pile in piles:
+            hours += (pile + speed - 1) // speed
+        return hours <= h
