6b9e42ccecf7285107fd153fbf06b9f3addbc0fb
Gauravesh Sharma <gauravesh.is20@bmsce.ac.in>
2023-11-18 00:02:23 +0530

    Update Maximum Depth of Binary Tree.py
    USing BFS

--- neetcode/Tree/Maximum Depth of Binary Tree.py
+++ neetcode/Tree/Maximum Depth of Binary Tree.py
@@ -1,8 +1,11 @@
+from collections import deque
+
 # Definition for a binary tree node.
 # class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
+
 #         self.right = right
 class Solution(object):
     def maxDepth(self, root):
@@ -12,8 +15,16 @@ class Solution(object):
         """
         if not root:
             return 0
-        if not root.left and  not root.right:
-            return 1
-        else:
-            return 1+max(self.maxDepth(root.left),self.maxDepth(root.right))
-        
+        #BFS
+        q=deque([root])
+        level = 0
+        while q:
+            for i in range(len(q)):
+                node=q.popleft()
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+
+            level+=1
+        return level
