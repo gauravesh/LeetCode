dd73589fbc042c31322a4ba11a7703af0ab1878e
Gauravesh Sharma <gauravesh.is20@bmsce.ac.in>
2023-11-23 02:24:31 +0530

    Update selling stock.py

--- neetcode/Sliding Window/selling stock.py
+++ neetcode/Sliding Window/selling stock.py
@@ -1,11 +1,20 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        min_list=[]
+        if len(prices) is( 0 or 1 ):
+            return 0
+    
         max_val=0
-        for i in range(len(prices)):
-            for j in range(i,len(prices)):
-                value=prices[j]-prices[i]
-                if value >= max_val:
-                    max_val=value
-        return max_val
+        mn=0
+        mx=1
+        value_list=list()
 
+        while (mx != len(prices)):
+            if (prices[mx] < prices[mn]):
+                mn=mx
+            else:
+                value_list.append(prices[mx]-prices[mn])
+                mx+=1
+        return max(value_list)
+        
+
+        
