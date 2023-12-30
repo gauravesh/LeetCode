214287e1418e6c1cf6c7417986a1d37eb9071e26
Gauravesh Sharma <gauravesh.is20@bmsce.ac.in>
2023-12-31 02:28:05 +0530

    Update Subtree.py

--- neetcode/Tree/Subtree.py
+++ neetcode/Tree/Subtree.py
@@ -1,19 +1,15 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+
 class Solution:
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        def sametree(n1,n2):
-          if not n1 and n2:
-            return True
-          if not n1 or n2:
-            return False
-          return ((n1.val == n2.val) and sametree(n1.right,n2.right) and sametree(n1.left,n2.left))
         if not root:
-          return False
-        if sametree(root,subRoot):
-          return True
-        return self.isSubtree(root.left,subRoot) or self.isSubtree(root.right,subRoot)
+            return False
+        if self.is_same_tree(root, subRoot):
+            return True
+        return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
+
+    def is_same_tree(self, s, t):
+        if not s and not t:
+            return True
+        if not s or not t:
+            return False
+        return s.val == t.val and self.is_same_tree(s.left, t.left) and self.is_same_tree(s.right, t.right)
