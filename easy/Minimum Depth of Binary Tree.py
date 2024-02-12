d3ac29c673efba31da40533c7f20d0937a442e8e
Gauravesh Sharma <gauravesh.is20@bmsce.ac.in>
2024-02-12 21:33:23 +0530

    Update Minimum Depth of Binary Tree.py

--- easy/Minimum Depth of Binary Tree.py
+++ easy/Minimum Depth of Binary Tree.py
@@ -1,18 +1,42 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
     def minDepth(self, root: Optional[TreeNode]) -> int:
         if not root:
             return 0
+        def preorder(node):
+            li=list()
+            if node is None:
+                return []
+            li.append(node.val)
+            if node.left:
+                li+=preorder(node.left)
+            elif not node.left:
+                li.append(999)
+            if node.right:
+                li+=preorder(node.right)
+            elif not node.right:
+                li.append(999)
+            return li
+        ss=preorder(root)
+        final_length=0
+        print(ss)
+    
+
+
+        for index in range(len(ss)):
+            if ss[index] == 999 and ss[index+1] == 999:
+                
+                temp=ss[:index]
+                print(temp)
+                c=temp.count(999)
+                print(c)
+                final_length=len(temp)-c
+                break
+        return final_length
+
         
-        def preorder(node, depth):
-            if not node:
-                return float('inf')  # Return infinity if the node is None
-            
-            if not node.left and not node.right:
-                return depth  # Return depth if it's a leaf node
-            
-            left_depth = preorder(node.left, depth + 1)  # Traverse left subtree
-            right_depth = preorder(node.right, depth + 1)  # Traverse right subtree
-            
-            return min(left_depth, right_depth)  # Return the minimum depth
-            
-        return preorder(root, 1)  # Start the traversal from the root with initial depth 1
