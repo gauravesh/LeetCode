f724b6a9377b5770458668e2bb398c927b2fdff2
Gauravesh Sharma <gauravesh.is20@bmsce.ac.in>
2024-02-26 15:54:47 +0530

    Update Construct Binary Tree from Preorder and Inorder Traversal.py

--- neetcode/Tree/Construct Binary Tree from Preorder and Inorder Traversal.py
+++ neetcode/Tree/Construct Binary Tree from Preorder and Inorder Traversal.py
@@ -9,8 +9,8 @@ class Solution:
         if not preorder or not inorder:
             return None
         root=TreeNode(preorder[0])
-        mid=inorder.index(preorder[0]) 
-        root.left=self.buildTree(preorder[:mid+1],inorder[:mid])
+        mid=(inorder.index(root.val))
+        root.left=self.buildTree(preorder[1:mid+1],inorder[:mid])
         root.right=self.buildTree(preorder[mid+1:],inorder[mid+1:])
         return root
         
