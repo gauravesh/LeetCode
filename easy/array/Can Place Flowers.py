2b6417077364e7a4537a4ab762bd3cc51da6309b
Gauravesh Sharma <gauravesh.is20@bmsce.ac.in>
2024-03-14 23:54:00 +0530

    Update Can Place Flowers.py

--- easy/array/Can Place Flowers.py
+++ easy/array/Can Place Flowers.py
@@ -5,21 +5,13 @@ class Solution(object):
         :type n: int
         :rtype: bool
         """
+        flowerbed=[0]+flowerbed+[0]
 
-        for i in range(len(flowerbed)-1):
-            if flowerbed[i] == 1  and flowerbed[i+1] == 1:
-                return False
-            elif flowerbed[i] == 1 and flowerbed[i+1] == 0:
-                continue
-            elif flowerbed[i]==0 and flowerbed[i+1] == 1:
-                continue
-            else :
-                flowerbed[i+1] = 1
+        for i in range(1,len(flowerbed)-1):
+            if flowerbed[i-1] == 0 and flowerbed[i+1] == 0 and flowerbed[i]==0:
+                flowerbed[i] = 1
                 n=n-1
-                continue
-        print(flowerbed)
-        print(n)
-        if n != 0:
-            return False
-        return True
-        
+                if n == 0:
+                    return True
+                    
+        return n<=0
